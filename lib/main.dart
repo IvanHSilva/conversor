ff45f0bcfa2c6bf209acbdf3b6d634fa47325ec6
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2024-03-16 16:20:24 -0300

    Minor corrections

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-const request = "https://api.hgbrasil.com/finance?format=json&key=99c2efe3";
+const request = "https://api.hgbrasil.com/finance?format=json&key=XXXXXXXX";
 
 void main() async {
   //print(await getData());
